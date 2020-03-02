1c746a0d07c15bdfd890c431dc55118141468b70
Sanjeev Sharma <29539278+thesanjeevsharma@users.noreply.github.com>
2020-03-02 12:17:52 +0530

    feat: Added mongoose schema

---
+++ server/models/ingredient.model.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const IngredientSchema = new mongoose.Schema({
+    valid: {
+        type : Boolean,
+        default : false
+    },
+    name : {
+        type: String,
+        required: ['Ingredient name is required.']
+    },
+    image : {
+        type : String
+    },
+    processings : [{
+        type : mongoose.Schema.Types.ObjectId,
+        ref : 'Processing'
+    }],
+}, {
+    timestamps : true
+});
+
+module.exports = mongoose.model('Ingredient', IngredientSchema);

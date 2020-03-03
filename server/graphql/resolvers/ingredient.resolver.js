9258a842d919cfd8c3beba20a500ecf8c2334540
Sanjeev Sharma <29539278+thesanjeevsharma@users.noreply.github.com>
2020-03-03 06:14:28 +0530

    feat: Ingredient resolvers

---
+++ server/graphql/resolvers/ingredient.resolver.js
@@ -0,0 +1,25 @@
+const Ingredient = require('../../models/ingredient.model');
+const Processing = require('../../models/processing.model');
+const Sachet = require('../../models/sachet.model');
+
+module.exports = {
+    ingredients: async () => {
+        try {
+            const ingredients = await Ingredient.find();
+            return {ingredients};
+        } catch(err) {
+            throw err;
+        }
+    },
+    createIngredient: async (args) => {
+        try {
+            const ingredient = new Ingredient({
+                name : args.input.name
+            });
+            const doc = await ingredient.save();
+            return {ingredient : doc};
+        } catch(err) {
+            throw err;
+        }
+    }
+}

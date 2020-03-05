065575b7b14ec1063277591945c0520458a4b305
siddhantk232 <siddhantk232@gmail.com>
2020-03-05 18:52:24 +0530

    feat(addRecipe): changed the behaviour of recipeName change as described below. feat(addRecipe): handling the recipe name input onChange feat(addRecipe): handling the tab name change onBlur

--- client/src/views/Forms/RecipeForm/index.js
+++ client/src/views/Forms/RecipeForm/index.js
@@ -1,96 +1,101 @@
-import React, { useState, useContext } from 'react';
-import { Input, TextButton, RadioGroup, ButtonTile } from '@dailykit/ui/';
+import React, { useState, useContext } from 'react'
+import { Input, TextButton, RadioGroup, ButtonTile } from '@dailykit/ui/'
 
-import { Context } from '../../../store/tabs/index';
+import { Context } from '../../../store/tabs/index'
 
-import { ViewWrapper } from '../../../components/Styled/ViewWrapper';
-import { RecipeActions, RecipeType, RecipePhotos } from './styled';
+import { ViewWrapper } from '../../../components/Styled/ViewWrapper'
+import { RecipeActions, RecipeType, RecipePhotos } from './styled'
 
-import AddIngredients from './AddIngredients';
-import Menu from '../../../components/Menu';
+import AddIngredients from './AddIngredients'
+import Menu from '../../../components/Menu'
 
 export default function AddRecipeForm() {
-	const [recipeName, setrecipeName] = useState('');
-	const { dispatch } = useContext(Context);
+   const [recipeName, setrecipeName] = useState('')
+   const { dispatch } = useContext(Context)
 
-	const options = [
-		{ id: 1, title: 'Vegetarian' },
-		{ id: 2, title: 'Non-Vegetarian' },
-		{ id: 3, title: 'Vegan' }
-	];
+   const options = [
+      { id: 1, title: 'Vegetarian' },
+      { id: 2, title: 'Non-Vegetarian' },
+      { id: 3, title: 'Vegan' }
+   ]
 
-	const handlePublish = () => {
-		console.log(recipeName);
-	};
+   const handlePublish = () => {
+      console.log(recipeName)
+   }
 
-	const handleRecipeNameChange = e => {
-		const title = e.target.value;
-		if (title.length > 0) {
-			dispatch({
-				type: 'SET_RECIPE_TITLE',
-				payload: { title, type: 'forms', view: 'recipe' }
-			});
-		} else {
-			dispatch({
-				type: 'SET_RECIPE_TITLE',
-				payload: { title: 'Untitled Recipe', type: 'forms', view: 'recipe' }
-			});
-		}
-		setrecipeName(title);
-	};
+   const handleRecipeNameChange = e => {
+      const title = e.target.value
 
-	return (
-		<ViewWrapper>
-			<Menu>
-				<div>
-					<Input
-						label="Untitled Recipe"
-						type="text"
-						name="recipeName"
-						value={recipeName}
-						onChange={handleRecipeNameChange}
-					/>
-				</div>
+      setrecipeName(title)
+   }
 
-				<RecipeActions>
-					<TextButton type="ghost" style={{ margin: '0px 10px' }}>
-						open in editor
-					</TextButton>
+   const handleTabNameChange = title => {
+      if (title.length > 0) {
+         dispatch({
+            type: 'SET_RECIPE_TITLE',
+            payload: { title, type: 'forms', view: 'recipe' }
+         })
+      } else {
+         dispatch({
+            type: 'SET_RECIPE_TITLE',
+            payload: { title: 'Untitled Recipe', type: 'forms', view: 'recipe' }
+         })
+      }
+   }
 
-					<TextButton type="ghost" style={{ margin: '0px 10px' }}>
-						save and exit
-					</TextButton>
+   return (
+      <ViewWrapper>
+         <Menu>
+            <div>
+               <Input
+                  label='Untitled Recipe'
+                  type='text'
+                  name='recipeName'
+                  value={recipeName}
+                  onChange={handleRecipeNameChange}
+                  onBlur={() => handleTabNameChange(recipeName)}
+               />
+            </div>
 
-					<TextButton
-						onClick={handlePublish}
-						type="solid"
-						style={{ margin: '0px 10px' }}
-					>
-						Publish
-					</TextButton>
-				</RecipeActions>
-			</Menu>
+            <RecipeActions>
+               <TextButton type='ghost' style={{ margin: '0px 10px' }}>
+                  open in editor
+               </TextButton>
 
-			{/* TODO: add stats here */}
+               <TextButton type='ghost' style={{ margin: '0px 10px' }}>
+                  save and exit
+               </TextButton>
 
-			<RecipeType>
-				<RadioGroup
-					options={options}
-					active={2}
-					onChange={option => console.log(option)}
-				/>
-			</RecipeType>
+               <TextButton
+                  onClick={handlePublish}
+                  type='solid'
+                  style={{ margin: '0px 10px' }}
+               >
+                  Publish
+               </TextButton>
+            </RecipeActions>
+         </Menu>
 
-			<RecipePhotos>
-				<ButtonTile
-					type="primary"
-					size="lg"
-					text="Add photos to your recipe"
-					helper="upto 1MB &#8226; only JPGs, PNGs, and PDFs are allowed."
-				/>
+         {/* TODO: add stats here */}
 
-				<AddIngredients />
-			</RecipePhotos>
-		</ViewWrapper>
-	);
+         <RecipeType>
+            <RadioGroup
+               options={options}
+               active={2}
+               onChange={option => console.log(option)}
+            />
+         </RecipeType>
+
+         <RecipePhotos>
+            <ButtonTile
+               type='primary'
+               size='lg'
+               text='Add photos to your recipe'
+               helper='upto 1MB &#8226; only JPGs, PNGs, and PDFs are allowed.'
+            />
+
+            <AddIngredients />
+         </RecipePhotos>
+      </ViewWrapper>
+   )
 }
